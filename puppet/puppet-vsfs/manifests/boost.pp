70ad0fff73db2ee35dc2e7cb3d3dfe3de00e2b6f
Lei Xu <eddyxu@gmail.com>
2013-03-21 04:33:45 -0500

    + Added scientific linux to boost

--- puppet/puppet-vsfs/manifests/boost.pp
+++ puppet/puppet-vsfs/manifests/boost.pp
@@ -6,26 +6,32 @@ class boost {
   $download_url = 'http://superb-dca3.dl.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.tar.bz2'
   $cwd = '/usr/local/src/boost_1_53_0'
 
-  if $operatingsystem == centos {
-    exec { 'download_boost':
-      command => "/usr/bin/wget ${download_url} -O- | tar -xj",
-      cwd     => '/usr/local/src',
-      require => Package['wget'],
-    }
+  case $operatingsystem {
+    centos, Scientific: {
+      exec { 'download_boost':
+        command => "/usr/bin/wget ${download_url} -O- | tar -xj",
+        cwd     => '/usr/local/src',
+        require => Package['wget'],
+      }
 
-    exec { 'configure_boost':
-      cwd     => $cwd,
-      command => "/bin/bash -c '${cwd}/bootstrap.sh -with-libraries=filesystem,serialization,system'",
-      require => [Exec['download_boost'], Class['cpp']],
-      provider => shell,
-    }
+      exec { 'configure_boost':
+        cwd      => $cwd,
+        command  => "/bin/bash -c '${cwd}/bootstrap.sh -with-libraries=filesystem,serialization,system'",
+        require  => [Exec['download_boost'], Class['cpp']],
+        provider => shell,
+      }
 
-    exec { 'build_boost':
-      cwd     => $cwd,
-      command => "/bin/bash -c '${cwd}/b2 threading=multi install'", 
-      require => Exec["configure_boost"],
+      exec { 'build_boost':
+        cwd     => $cwd,
+        command => "/bin/bash -c '${cwd}/b2 threading=multi install'",
+        require => Exec["configure_boost"],
+      }
+    }
+    ubuntu: {
+      package { ['libboost-filesystem1.50-dev',
+        'libboost-serialization1.50-dev']:
+        ensure => installed,
+      }
     }
   }
 }
-
-include boost
